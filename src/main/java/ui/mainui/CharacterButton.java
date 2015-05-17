7b1a5f06e6fef1dd08bf5af658af62291d22ef75
管梦秋 <634880846@qq.com>
2015-05-17 14:44:10 +0800

    .

--- src/main/java/ui/mainui/CharacterButton.java
+++ src/main/java/ui/mainui/CharacterButton.java
@@ -8,17 +8,19 @@ public class CharacterButton extends JButton{
 	
 	public CharacterButton(String text){
 		this.setText(text);
+
+	}
+	
+	public CharacterButton(char c){
+		this.setText(String.valueOf(c));
+	}
+	
+	private void figure(){
 		this.setBorder(null);
 		this.setForeground(Color.white);
 		this.setBackground(new Color(87,89,91));
 	}
 	
-	public CharacterButton(char c){
-		this.setText(String.valueOf(c));
-		this.setBorder(null);
-		this.setForeground(Color.white);
-		this.setBackground(new Color(87,89,91));
-	}
 
 
 }
