a83f40738bcf637546856fa75b20e696838b7388
管梦秋 <634880846@qq.com>
2015-05-28 20:27:43 +0800

    青共校下课啦

--- src/main/java/ui/playerui/PlayerPanel.java
+++ src/main/java/ui/playerui/PlayerPanel.java
@@ -4,9 +4,11 @@ import java.awt.Button;
 import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Vector;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 import bl.teambl.TeamController;
@@ -30,7 +32,7 @@ public class PlayerPanel extends JPanel{
 	private static JPanel HeaderPanel(){
 		JPanel headerPanel = new JPanel();
 		headerPanel.setLayout(null);
-		headerPanel.setBounds(0,0,FrameSize.width,60);
+		headerPanel.setBounds(0,0,FrameSize.width,30);
 		headerPanel.setBackground(new Color(87,89,91));
 		
 		//根据首字母查找球员
@@ -45,10 +47,10 @@ public class PlayerPanel extends JPanel{
 		}
 		
 		//根据姓名查找球员
-		EditableTextField playerNameTextField = new EditableTextField("根据球员姓名查找");
+		EditableTextField playerNameTextField = new EditableTextField("按姓名查找");
 		playerNameTextField.setBackground(new Color(69,69,69));
 		playerNameTextField.setForeground(Color.white);
-		playerNameTextField.setBounds(27*30+50,0,150,30);
+		playerNameTextField.setBounds(27*30,0,(FrameSize.width-27*30)/2-10,30);
 		playerNameTextField.addMouseListener(new MouseAdapter(){
 			public void mouseClicked(MouseEvent e){
 				playerNameTextField.setText("");
@@ -58,21 +60,42 @@ public class PlayerPanel extends JPanel{
 		
 		//根据球队查找球员
 		String team[] = new String[30];//所有球队名
-		MyComboBox findPlayerAccordingTeam = new MyComboBox("根据球队查找",team);
-		findPlayerAccordingTeam.setBounds(0,30,150,30);
+		MyComboBox findPlayerAccordingTeam = new MyComboBox("按球队查找",team);
+		findPlayerAccordingTeam.setBounds(27*30+(FrameSize.width-27*30)/2,0,FrameSize.width-(27*30+(FrameSize.width-27*30)/2),30);
 		headerPanel.add(findPlayerAccordingTeam);
-		
+
+/*
 		//根据球员位置筛选
 		MyComboBox screenPlayerAccordingLocation = new MyComboBox("根据球员位置筛选",new String[]{"F","C","G"});
 		screenPlayerAccordingLocation.setBounds(300,30,150,30);
 		headerPanel.add(screenPlayerAccordingLocation);
 		
 		//根据球员联盟筛选
-		MyComboBox screenPlayerAccordingZone = new MyComboBox("根据赛区筛选",new String[]{"E-ATLANTIC","E-CENTRAL","E-SOUTHEAST","W-PACIFIC","W-SOUTHWEST","W-SOUTHEAST"});
-		screenPlayerAccordingLocation.setBounds(500,30,150,30);
+		MyComboBox screenPlayerAccordingZone = new MyComboBox("根据球员联盟筛选",new String[]{"EAST","E-ATLANTIC","E-CENTRAL","E-SOUTHEAST","WEST","W-PACIFIC","W-SOUTHWEST","W-SOUTHEAST"});
+		screenPlayerAccordingZone.setBounds(500,30,150,30);
 		headerPanel.add(screenPlayerAccordingZone);
 		
 		//根据排序依据筛选
+		MyComboBox screenPlayerAccordingScreen = new MyComboBox("根据排序依据筛选",new String[] { "得分", "篮板", "助攻",
+				"得分/篮板/助攻", "盖帽", "抢断", "犯规", "失误", "分钟", "效率", "投篮", "三分",
+				"罚球", "两双" });
+		screenPlayerAccordingScreen.setBounds(700,30,150,30);
+		headerPanel.add(screenPlayerAccordingScreen);
+*/
 		return headerPanel;
+	}
+
+	private JScrollPane TablePanel(){
+		Vector columnsName = new Vector();
+		columnsName.add(" ");
+		columnsName.add("球员");
+		columnsName.add("姓名");
+		columnsName.add("球队");
+		columnsName.add("位置");
+		
+//		Vector data = new Vector();
+		
+		return null;
+		
 	}
 }
