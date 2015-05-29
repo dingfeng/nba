6a74b8f9737649aa8cc05c4c91df01d96fe2159f
管梦秋 <634880846@qq.com>
2015-05-29 16:49:56 +0800

    球去打球啦

--- src/main/java/ui/statistics/StatisticsPlayerPanel.java
+++ src/main/java/ui/statistics/StatisticsPlayerPanel.java
@@ -1,11 +1,16 @@
 package ui.statistics;
 
 import java.awt.Color;
+import java.util.Vector;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
 
 import ui.mainui.FrameSize;
 import ui.mainui.MyComboBox;
+import ui.mainui.MyTable;
+import vo.PlayerMatchVO;
 
 public class StatisticsPlayerPanel extends JPanel {
 	public StatisticsPlayerPanel() {
@@ -15,8 +20,11 @@ public class StatisticsPlayerPanel extends JPanel {
 		this.setOpaque(false);
 		JPanel headerPanel = HeaderPanel();
 		this.add(headerPanel);
+		JScrollPane low = LowTable(null);
+		this.add(low);
 	}
 
+	/**筛选栏*/
 	private static JPanel HeaderPanel() {
 		JPanel headerPanel = new JPanel();
 		headerPanel.setLayout(null);
@@ -60,4 +68,79 @@ public class StatisticsPlayerPanel extends JPanel {
 		return headerPanel;
 	}
 
+	/**低阶表格*/
+	private JScrollPane LowTable(PlayerMatchVO[] playerMatchVOs){
+		if(playerMatchVOs!=null){
+			Vector columnsName = new Vector();
+			columnsName.add("排名");
+			columnsName.add("球员");
+			columnsName.add("场数");
+			columnsName.add("球队");
+			columnsName.add("得分");
+			columnsName.add("篮板");
+			columnsName.add("助攻");
+			columnsName.add("盖帽");
+			columnsName.add("抢断");
+			columnsName.add("犯规");
+			columnsName.add("失误");
+			columnsName.add("分钟");
+			columnsName.add("效率");
+			columnsName.add("投篮%");
+			columnsName.add("三分%");
+			columnsName.add("罚球%");
+			columnsName.add("两双");
+
+		}
+		else{
+			Vector columnsName = new Vector();
+			columnsName.add("排名");
+			columnsName.add("球员");
+			columnsName.add("球队");
+			columnsName.add("场数");
+			columnsName.add("得分");
+			columnsName.add("篮板");
+			columnsName.add("助攻");
+			columnsName.add("盖帽");
+			columnsName.add("抢断");
+			columnsName.add("犯规");
+			columnsName.add("失误");
+			columnsName.add("分钟");
+			columnsName.add("效率");
+			columnsName.add("投篮%");
+			columnsName.add("三分%");
+			columnsName.add("罚球%");
+			columnsName.add("两双");
+			
+			Vector data = new Vector();
+			for(int i=0;i<100;i++){
+				Vector rowData = new Vector();
+				rowData.add(100);
+				rowData.add("AbCdEfG");
+				rowData.add("aBcDeFg");
+				rowData.add(100);
+				rowData.add(400);
+				rowData.add(100);
+				rowData.add(100);
+				rowData.add(100);
+				rowData.add(100);
+				rowData.add(100);
+				rowData.add(100);
+				rowData.add(100);
+				rowData.add(100);
+				rowData.add(99.9);
+				rowData.add(99.9);
+				rowData.add(99.9);
+				rowData.add(100);
+				
+				data.add(rowData);
+			}
+			DefaultTableModel table = new DefaultTableModel(data, columnsName);
+			MyTable myTable = new MyTable(table);
+			JScrollPane jScrollPane = new JScrollPane(myTable);
+			jScrollPane.setBounds(0, 40, FrameSize.width,
+					FrameSize.height* 7 / 8 - 40);
+			return jScrollPane;
+		}
+		return null;
+	}
 }
