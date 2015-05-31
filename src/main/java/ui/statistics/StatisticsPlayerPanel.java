e1cbdbb9ba5ea3c6aa2c0c6ce36daef0f0362b60
管梦秋 <634880846@qq.com>
2015-05-31 14:01:27 +0800

    昨天的表格不见了

--- src/main/java/ui/statistics/StatisticsPlayerPanel.java
+++ src/main/java/ui/statistics/StatisticsPlayerPanel.java
@@ -1,11 +1,17 @@
 package ui.statistics;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.util.Comparator;
 import java.util.Vector;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 import ui.mainui.FrameSize;
 import ui.mainui.MyComboBox;
@@ -13,6 +19,13 @@ import ui.mainui.MyTable;
 import vo.PlayerMatchVO;
 
 public class StatisticsPlayerPanel extends JPanel {
+
+	Vector<String> columnsName = new Vector<String>();
+	Vector data = new Vector();
+	DefaultTableModel table = new DefaultTableModel(data, columnsName);
+	MyTable mytable = new MyTable(table);
+	JScrollPane jScrollPane = new JScrollPane(mytable);
+
 	public StatisticsPlayerPanel() {
 		this.setLayout(null);
 		this.setBounds(0, 0, FrameSize.width, FrameSize.height * 7 / 8);
@@ -20,62 +33,106 @@ public class StatisticsPlayerPanel extends JPanel {
 		this.setOpaque(false);
 		JPanel headerPanel = HeaderPanel();
 		this.add(headerPanel);
-		JScrollPane low = LowTable(null);
-		this.add(low);
+		setLowTable(null);
 	}
 
-	/**筛选栏*/
+	/** 筛选栏 */
 	private static JPanel HeaderPanel() {
 		JPanel headerPanel = new JPanel();
 		headerPanel.setLayout(null);
 		headerPanel.setBounds(0, 0, FrameSize.width, 40);
 		headerPanel.setBackground(new Color(87, 89, 91));
 
+		// 根据球员位置筛选
+		MyComboBox screenPlayerAccordingLocation = new MyComboBox("球员位置",
+				new String[] { "F", "C", "G" });
+		screenPlayerAccordingLocation.setBounds(0, 5, 150, 30);
+		headerPanel.add(screenPlayerAccordingLocation);
 
-		  //根据球员位置筛选 
-		  MyComboBox screenPlayerAccordingLocation = new MyComboBox("球员位置",new String[]{"F","C","G"});
-		  screenPlayerAccordingLocation.setBounds(0,5,150,30);
-		  headerPanel.add(screenPlayerAccordingLocation);
-		  
-		  //根据球员联盟筛选 
-		  MyComboBox screenPlayerAccordingZone = new MyComboBox("球员联盟",new
-		  String[]{"EAST","E-ATLANTIC","E-CENTRAL","E-SOUTHEAST"
-		  ,"WEST","W-PACIFIC","W-SOUTHWEST","W-SOUTHEAST"});
-		  screenPlayerAccordingZone.setBounds(150,5,150,30);
-		  headerPanel.add(screenPlayerAccordingZone);
-		  
-		  //根据排序依据筛选 
-		  MyComboBox screenPlayerAccordingScreen = new MyComboBox("排序依据",new String[] { "得分", "篮板", "助攻", "得分/篮板/助攻",
-		  "盖帽", "抢断", "犯规", "失误", "分钟", "效率", "投篮", "三分", "罚球", "两双" });
-		  screenPlayerAccordingScreen.setBounds(300,5,150,30);
-		  headerPanel.add(screenPlayerAccordingScreen);
-		  
-		  //根据赛季筛选
-		  MyComboBox screenPlayerAccordingSeason = new MyComboBox("赛季",new String[]{"2013-2014","2014-2015"});
-		  screenPlayerAccordingSeason.setBounds(450,5,150,30);
-		  headerPanel.add(screenPlayerAccordingSeason);
-		  
-		  //场均or总数
-		  MyComboBox aveOrAll= new MyComboBox(new String[]{"场均","总数"});
-		  aveOrAll.setBounds(600,5,150,30);
-		  headerPanel.add(aveOrAll);
-		  
-		  //低阶or高阶
-		  MyComboBox lowOrHigh= new MyComboBox(new String[]{"低阶","高阶"});
-		  lowOrHigh.setBounds(750,5,150,30);
-		  headerPanel.add(lowOrHigh);
-		  
+		// 根据球员联盟筛选
+		MyComboBox screenPlayerAccordingZone = new MyComboBox("球员联盟",
+				new String[] { "EAST", "E-ATLANTIC", "E-CENTRAL",
+						"E-SOUTHEAST", "WEST", "W-PACIFIC", "W-SOUTHWEST",
+						"W-SOUTHEAST" });
+		screenPlayerAccordingZone.setBounds(150, 5, 150, 30);
+		headerPanel.add(screenPlayerAccordingZone);
+
+		// 根据赛季筛选
+		MyComboBox screenPlayerAccordingSeason = new MyComboBox("赛季",
+				new String[] { "2013-2014", "2014-2015" });
+		screenPlayerAccordingSeason.setBounds(450, 5, 150, 30);
+		headerPanel.add(screenPlayerAccordingSeason);
+
+		// 场均or总数
+		MyComboBox aveOrAll = new MyComboBox(new String[] { "场均", "总数" });
+		aveOrAll.setBounds(600, 5, 150, 30);
+		headerPanel.add(aveOrAll);
+
+		// 低阶or高阶
+		MyComboBox lowOrHigh = new MyComboBox(new String[] { "低阶", "高阶" });
+		lowOrHigh.setBounds(750, 5, 150, 30);
+		headerPanel.add(lowOrHigh);
+
 		return headerPanel;
 	}
 
-	/**低阶表格*/
-	private JScrollPane LowTable(PlayerMatchVO[] playerMatchVOs){
-		if(playerMatchVOs!=null){
-			Vector columnsName = new Vector();
+	/** 低阶表格 */
+	private void setLowTable(PlayerMatchVO[] playerMatchVOs) {
+		if (playerMatchVOs != null) {
+			columnsName.removeAllElements();
+			/* 00排名 */columnsName.add("排名");
+			/* 01球员 */columnsName.add("球员");
+			/* 02姓名 */columnsName.add("姓名");
+			/* 03球队 */columnsName.add("球队");
+			/* 04场数 */columnsName.add("场数");
+			/* 05篮板 */columnsName.add("篮板");
+			/* 06助攻 */columnsName.add("助攻");
+			/* 07分钟 */columnsName.add("分钟");
+			/* 08效率 */columnsName.add("效率");
+			/* 09三分命中率 */columnsName.add("三分%");
+			/* 10罚球命中率 */columnsName.add("罚球%");
+			/* 11投篮命中率 */columnsName.add("投篮%");
+			/* 12进攻 */columnsName.add("进攻");
+			/* 13防守 */columnsName.add("防守");
+			/* 14抢断 */columnsName.add("抢断");
+			/* 15盖帽 */columnsName.add("盖帽");
+			/* 16失误 */columnsName.add("失误");
+			/* 17犯规 */columnsName.add("犯规");
+			/* 18两双 */columnsName.add("两双");
+			/* 19得分 */columnsName.add("得分");
+
+			data.clear();
+			for (int i = 0; i < playerMatchVOs.length; i++) {
+				Vector rowData = new Vector();
+				/* 00排名 */rowData.add(i + 1);
+				/* 01球员 */rowData.add("图片");
+				/* 02姓名 */rowData.add(playerMatchVOs[i].getName());
+				/* 03球队 */rowData.add(playerMatchVOs[i].getTeam());
+				/* 04场数 */rowData.add(playerMatchVOs[i].getMatchNo());
+				/* 05篮板 */rowData.add(playerMatchVOs[i].getRebs());
+				/* 06助攻 */rowData.add(playerMatchVOs[i].getHelp());
+				/* 07分钟 */rowData.add(playerMatchVOs[i].getMinute());
+				/* 08效率 */rowData.add(playerMatchVOs[i].getEfficiency());
+				/* 09三分命中率 */rowData.add(playerMatchVOs[i].getThreeHitRate());
+				/* 10罚球命中率 */rowData.add(playerMatchVOs[i].getPenaltyHitRate());
+				/* 11投篮命中率 */rowData.add(playerMatchVOs[i].getHitRate());
+				/* 12进攻 */rowData.add(playerMatchVOs[i].getOffendNo());
+				/* 13防守 */rowData.add(playerMatchVOs[i].getDefenceNo());
+				/* 14抢断 */rowData.add(playerMatchVOs[i].getStealsNo());
+				/* 15盖帽 */rowData.add(playerMatchVOs[i].getBlockNo());
+				/* 16失误 */rowData.add(playerMatchVOs[i].getMistakesNo());
+				/* 17犯规 */rowData.add(playerMatchVOs[i].getFoulsNo());
+				/* 18两双 */rowData.add(playerMatchVOs[i].getTwoPair());
+				/* 19得分 */rowData.add(playerMatchVOs[i].getPoints());
+				data.add(rowData);
+			}
+			setScrollPane();
+		} else {
+			columnsName.removeAllElements();
 			columnsName.add("排名");
 			columnsName.add("球员");
-			columnsName.add("场数");
 			columnsName.add("球队");
+			columnsName.add("场数");
 			columnsName.add("得分");
 			columnsName.add("篮板");
 			columnsName.add("助攻");
@@ -90,57 +147,85 @@ public class StatisticsPlayerPanel extends JPanel {
 			columnsName.add("罚球%");
 			columnsName.add("两双");
 
-		}
-		else{
-			Vector columnsName = new Vector();
-			columnsName.add("排名");
-			columnsName.add("球员");
-			columnsName.add("球队");
-			columnsName.add("场数");
-			columnsName.add("得分");
-			columnsName.add("篮板");
-			columnsName.add("助攻");
-			columnsName.add("盖帽");
-			columnsName.add("抢断");
-			columnsName.add("犯规");
-			columnsName.add("失误");
-			columnsName.add("分钟");
-			columnsName.add("效率");
-			columnsName.add("投篮%");
-			columnsName.add("三分%");
-			columnsName.add("罚球%");
-			columnsName.add("两双");
-			
-			Vector data = new Vector();
-			for(int i=0;i<100;i++){
+			data.clear();
+			for (int i = 0; i < 100; i++) {
 				Vector rowData = new Vector();
-				rowData.add(100);
-				rowData.add("AbCdEfG");
-				rowData.add("aBcDeFg");
-				rowData.add(100);
-				rowData.add(400);
-				rowData.add(100);
-				rowData.add(100);
-				rowData.add(100);
-				rowData.add(100);
-				rowData.add(100);
-				rowData.add(100);
-				rowData.add(100);
-				rowData.add(100);
-				rowData.add(99.9);
-				rowData.add(99.9);
-				rowData.add(99.9);
-				rowData.add(100);
-				
+				for (int j = 0; j < 20; j++)
+					rowData.add(100);
 				data.add(rowData);
 			}
-			DefaultTableModel table = new DefaultTableModel(data, columnsName);
-			MyTable myTable = new MyTable(table);
-			JScrollPane jScrollPane = new JScrollPane(myTable);
-			jScrollPane.setBounds(0, 40, FrameSize.width,
-					FrameSize.height* 7 / 8 - 40);
-			return jScrollPane;
+			setScrollPane();
 		}
-		return null;
+	}
+
+	/** 高阶表格 */
+	private void setHighTable(PlayerMatchVO[] playerMatchVOs) {
+
+	}
+
+	/** 设置jScrollPanel */
+	private void setScrollPane() {
+		table.setDataVector(data, columnsName);
+		mytable.updateUI();
+		jScrollPane
+				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		jScrollPane
+				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		jScrollPane.setBounds(0, 40, FrameSize.width,
+				FrameSize.height * 7 / 8 - 40);
+		jScrollPane.setBackground(Color.white);
+		jScrollPane.getViewport().setOpaque(false);
+		resizeTable(false,jScrollPane,mytable);
+		this.add(jScrollPane);
+		this.repaint();
+	}
+
+	/**设置表格*/
+	private void resizeTable(boolean bool, JScrollPane jsp, JTable table) {
+		Dimension containerwidth = null;
+		if (!bool) {
+			// 初始化时，父容器大小为首选大小，实际大小为0
+			containerwidth = jsp.getPreferredSize();
+		} else {
+			// 界面显示后，如果父容器大小改变，使用实际大小而不是首选大小
+			containerwidth = jsp.getSize();
+		}
+		// 计算表格总体宽度 getTable().
+		int allwidth = table.getIntercellSpacing().width;
+		for (int j = 0; j < table.getColumnCount(); j++) {
+			// 计算该列中最长的宽度
+			int max = 0;
+			for (int i = 0; i < table.getRowCount(); i++) {
+				int width = table
+						.getCellRenderer(i, j)
+						.getTableCellRendererComponent(table,
+								table.getValueAt(i, j), false, false, i, j)
+						.getPreferredSize().width;
+				if (width > max) {
+					max = width;
+				}
+			}
+			// 计算表头的宽度
+			int headerwidth = table
+					.getTableHeader()
+					.getDefaultRenderer()
+					.getTableCellRendererComponent(
+							table,
+							table.getColumnModel().getColumn(j).getIdentifier(),
+							false, false, -1, j).getPreferredSize().width;
+			// 列宽至少应为列头宽度
+			max += headerwidth;
+			// 设置列宽
+			table.getColumnModel().getColumn(j).setPreferredWidth(max);
+			// 给表格的整体宽度赋值，记得要加上单元格之间的线条宽度1个像素
+			allwidth += max + table.getIntercellSpacing().width;
+		}
+		allwidth += table.getIntercellSpacing().width;
+		// 如果表格实际宽度大小父容器的宽度，则需要我们手动适应；否则让表格自适应
+		if (allwidth > containerwidth.width) {
+			table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		} else {
+			table.setAutoResizeMode(JTable.AUTO_RESIZE_SUBSEQUENT_COLUMNS);
+		}
 	}
 }
