33880c150e1cdd7a41f8d7f92414d90f465a270f
Ning <547568644@qq.com>
2015-06-11 15:50:43 +0800

    赛季

--- src/main/java/ui/teamui/RecentMatchPanel.java
+++ src/main/java/ui/teamui/RecentMatchPanel.java
@@ -5,6 +5,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Vector;
 
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -13,6 +14,7 @@ import javax.swing.table.DefaultTableModel;
 
 import po.MatchesPO;
 import ui.mainui.FrameSize;
+import ui.mainui.MyComboBox;
 import ui.mainui.MyFrame;
 import ui.mainui.MyTable;
 import bl.matchbl.MatchController;
@@ -26,7 +28,7 @@ public class RecentMatchPanel extends JPanel {
 	Vector rowimage = new Vector();
 	DefaultTableModel table = new DefaultTableModel(rowimage, columnsName);
 	MyTable mytable = new MyTable(table);
-	
+	JComboBox season=new MyComboBox(new String[]{"2014","2013","2012","2011","2010","2009","2008","2007","2006","2005","2004","2003","2002","2001","2000","1999","1998","1997","1996","1995","1994","1993","1992","1991","1990","1989","1988","1987","1986","1985"});
 	JScrollPane recentjScrollPane;
 
 	public RecentMatchPanel() {
@@ -48,8 +50,12 @@ public class RecentMatchPanel extends JPanel {
 		recent.setOpaque(true);
 		recent.setBackground(FrameSize.bluecolor);
 		recent.setForeground(Color.white);
-		
+		season.setBounds(FrameSize.width-150,0 ,100 ,30 );
+		season.setBackground(Color.white);
+		season.addActionListener(e->setRecentTable());
+		season.setForeground(Color.black);
 		this.add(recent);
+		recent.add(season);
 
 	}
 
--- src/main/java/ui/teamui/TeamContrastPanel.java
+++ src/main/java/ui/teamui/TeamContrastPanel.java
@@ -2,22 +2,24 @@ package ui.teamui;
 
 import java.awt.Color;
 import java.awt.Image;
-import java.awt.event.InputEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.KeyStroke;
 
 import po.TeamPO;
 import ui.mainui.EditableTextField;
 import ui.mainui.FrameSize;
 import ui.mainui.MyButton;
+import ui.mainui.MyComboBox;
 import ui.mainui.MyFrame;
 import bl.teambl.TeamController;
 import blservice.teamblservice.Teamblservice;
@@ -31,6 +33,9 @@ public class TeamContrastPanel extends JPanel{
 	JLabel name1=new JLabel();
 	JLabel name2=new JLabel();
 	JTextField t2;
+	boolean one=true;
+	JComboBox seasontype=new MyComboBox(new String[]{"常规赛","季后赛"});
+	SeasonType season=SeasonType.REGULAR;
 	public TeamContrastPanel(){
 	this.setLayout(null);
 	this.setBounds(0, 0,
@@ -62,7 +67,13 @@ public class TeamContrastPanel extends JPanel{
 		pic2.setBounds(FrameSize.width/2+300, FrameSize.height/8, FrameSize.height/5, FrameSize.height/5);
 		name1.setBounds(FrameSize.width/2-300-FrameSize.height/5, FrameSize.height/3, 100, 50);
 		name2.setBounds(FrameSize.width/2+300, FrameSize.height/3, 100,50);
-		pic1.setOpaque(false);
+		pic2.addMouseListener(new show());
+		name2.addMouseListener(new show());
+		seasontype.setBounds(FrameSize.width/2,5,100,30);
+		seasontype.addActionListener(e->setOneOrTwo());
+		seasontype.setBackground(Color.white);
+		seasontype.setForeground(Color.black);
+		this.add(seasontype);
 		this.add(enter);
 		this.add(t2);
 		this.add(name1);
@@ -72,9 +83,11 @@ public class TeamContrastPanel extends JPanel{
 	}
 	
 	public void setChart(){
+		one=true;
 		String teamname=MyFrame.teampanel.nameAbridgeresult.getText();
 		TeamPO team=tc.getTeamData(teamname);
 		pic1.setIcon(FrameSize.scaleImage(new ImageIcon(team.getImage()),FrameSize.height/5, FrameSize.height/5));
+		pic2.setIcon(FrameSize.scaleImage(new ImageIcon("image/nbabig.png"),FrameSize.height/5, FrameSize.height/5));
 		name1.setText(team.getName());
 		name2.setText("NBA联盟平均");
 		new Thread()
@@ -82,7 +95,7 @@ public class TeamContrastPanel extends JPanel{
 			public void run()
 			{
 		
-		Image image=tc.getTeamBar(2014, teamname, SeasonType.REGULAR);
+		Image image=tc.getTeamBar(2014, teamname, season);
 		
 		imagelabel.setIcon(new ImageIcon(image));
 		imagelabel.setBounds((FrameSize.width-560)/2, 30, 560, 420);
@@ -98,6 +111,7 @@ public class TeamContrastPanel extends JPanel{
 	}
 
 	void setCompare(){
+		one=false;
 		String teamname=t2.getText();
 		try {
 			TeamPO team=tc.getTeamData(teamname);
@@ -110,7 +124,7 @@ public class TeamContrastPanel extends JPanel{
 		TeamPO team=tc.getTeamData(teamname);
 		name2.setText(team.getName());
 		pic2.setIcon(FrameSize.scaleImage(new ImageIcon(team.getImage()),FrameSize.height/5, FrameSize.height/5));
-		Image image=tc.getTeamCompare(2014, MyFrame.teampanel.nameAbridgeresult.getText(),teamname, SeasonType.REGULAR);
+		Image image=tc.getTeamCompare(2014, MyFrame.teampanel.nameAbridgeresult.getText(),teamname, season);
 		
 		imagelabel.setIcon(new ImageIcon(image));
 		this.add(name2);
@@ -118,5 +132,52 @@ public class TeamContrastPanel extends JPanel{
 		this.add(imagelabel);
 		this.repaint();
 		
+	}
+
+	class show implements MouseListener{
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			MyFrame.teampanel.showOne(name2.getText());
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+
+	void setOneOrTwo(){
+		if(seasontype.getSelectedItem().equals("常规赛")){
+			season=SeasonType.REGULAR;
+		}else{
+			season=SeasonType.PLAYOFF;
+		}
+		if(one){
+			setChart();
+		}else{
+			setCompare();
+		}
 	}
 }
